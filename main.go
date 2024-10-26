2700a8680f333408bb1692fa6caa4ca8ce6daf1b
skucukgul <selim.kucukgul@gmail.com>
2024-10-26 10:58:15 +0300

    if else

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	number := 50
+	guess := 34
+	if guess < 1 || guess > 100 {
 
-	a := []int{1, 2, 3}
-	fmt.Println(a)
-	b := a[:len(a)-1]
-	fmt.Println(b)
-
+		fmt.Println("The guess must be between 1 and 100")
+	} else {
+		if guess < number {
+			fmt.Println("Guess:", guess, "is below", number)
+		}
+		if guess > number {
+			fmt.Println("Guess:", guess, "is above", number)
+		}
+		if guess == number {
+			fmt.Println("Guess is equal to number")
+		}
+	}
 }
