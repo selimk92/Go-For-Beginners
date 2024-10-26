3e6265032d0b4ffaf063ba0bd6a42cce708439ae
skucukgul <selim.kucukgul@gmail.com>
2024-10-26 21:13:46 +0300

    panic first

--- main.go
+++ main.go
@@ -2,22 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 )
 
 func main() {
 
-	res, err := http.Get("http://www.google.com/robots.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	robots, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(robots))
-
+	a, b := 1, 0
+	ans := a / b
+	panic(ans)
+	fmt.Println(ans)
 }
