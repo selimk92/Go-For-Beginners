07e65f57c7d647abf77171f09e8a672c947e0ea4
skucukgul <selim.kucukgul@gmail.com>
2024-10-26 11:09:56 +0300

    switch-case-3

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	i := 5
-	switch {
-	case i <= 10:
-		fmt.Println("one")
-	case i <= 20:
-		fmt.Println("two")
-	case i == 5:
-		fmt.Println("three")
+	var i interface{} = 1.5
+	switch i.(type) {
+	case int:
+		fmt.Println("int")
+	case float64:
+		fmt.Println("float64")
+
+	case string:
+		fmt.Println("string")
 	default:
 		fmt.Println("default")
 
