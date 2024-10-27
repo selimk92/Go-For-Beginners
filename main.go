2810fa3f35037f7bbb04aa08c4e650d17413e4d1
skucukgul <selim.kucukgul@gmail.com>
2024-10-27 17:56:25 +0300

    method

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
+type insan struct {
+	isim string
+	yas  int
+}
 
-	func() {
-		fmt.Println("hello world")
-	}()
+func main() {
+	kisi := insan{"selim", 25}
+	kisi.tanıt()
+}
+
+func (i insan) tanıt() {
+	fmt.Println("Merhaba benim adım ", i.isim, "ve", i.yas, "yasındayım")
+
 }
